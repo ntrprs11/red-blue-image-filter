e4aa1295538813355056bf1d9a572a2164360abf
ntrprs11 <robin.o.krn@gmail.com>
2022-10-22 13:23:51 +0200

    first tests with displaying rgb in 3d axis

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("Test!")
+import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from matplotlib import colors
+from matplotlib import cm
+
+# Imagename Input by user and reading the image as "color"
+
+imginput = str(input("Please enter the entire filename (including .jpg, .png etc.): "))
+img = cv2.imread(imginput, 1)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+# Test
+r, g, b = cv2.split(img)
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1, projection="3d")
+pixel_colors = img.reshape((np.shape(img)[0]*np.shape(img)[1], 3))
+norm = colors.Normalize(vmin=-1.,vmax=1.)
+norm.autoscale(pixel_colors)
+pixel_colors = norm(pixel_colors).tolist()
+axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker=".")
+axis.set_xlabel("Red")
+axis.set_ylabel("Green")
+axis.set_zlabel("Blue")
+
+plt.show()
+
+
+
+# Saving the Image
+
+# cv2.imwrite("result.jpg", img)
